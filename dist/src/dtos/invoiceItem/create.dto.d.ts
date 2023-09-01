33d54c9a850c6ae9f2ad1ba19ea204b20d2f8cee
Rajeshtp0812 <109742942+Rajeshtp0812@users.noreply.github.com>
2023-09-01 10:00:26 +0530

    Delete dist directory

--- dist/src/dtos/invoiceItem/create.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class createInvoiceItemDto {
-    code: string;
-    description: string;
-    hasCode: string;
-    rate: number;
-    quantity: number;
-    amount: number;
-    invoiceId: number;
-}
--- dist/src/dtos/user/user.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class userDto {
-    username: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-}
